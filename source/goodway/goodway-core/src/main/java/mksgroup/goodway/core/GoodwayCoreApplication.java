0b3a6c36b8a6b9588af527816d1e59ef7eee3513
thachln <thachln@gmail.com>
2018-05-18 15:52:51 +0700

    Thêm module goodway-core: cung cấp API cho xử lý thuận toán lập kế hoạch.

---
+++ source/goodway/goodway-core/src/main/java/mksgroup/goodway/core/GoodwayCoreApplication.java
@@ -0,0 +1,12 @@
+package mksgroup.goodway.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodwayCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodwayCoreApplication.class, args);
+	}
+}
