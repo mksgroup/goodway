065feea57864ad118f2428bb29d65a3357161cfe
thachln <thachln@gmail.com>
2018-06-20 16:33:28 +0700

    Fix bug of Repository. Upgrade code for Unit Testing: Vehicle Repository.

--- source/goodway/goodway-domain/src/main/java/mksgroup/goodway/repository/DeliveryBatchRepository.java
+++ source/goodway/goodway-domain/src/main/java/mksgroup/goodway/repository/DeliveryBatchRepository.java
@@ -1,9 +1,11 @@
 package mksgroup.goodway.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import mksgroup.goodway.entity.DeliveryBatch;
 
+@Repository
 public interface DeliveryBatchRepository extends CrudRepository<DeliveryBatch, Long> {
     
 }
