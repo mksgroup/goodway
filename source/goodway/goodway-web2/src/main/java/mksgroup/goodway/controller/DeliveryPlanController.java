823b14165f0020a67f881cb6a95abd6671c9d219
ZoroMochi <32060631+vndkubi@users.noreply.github.com>
2018-06-21 07:32:21 +0700

    update page delivery-reusult.html

--- source/goodway/goodway-web2/src/main/java/mksgroup/goodway/controller/DeliveryPlanController.java
+++ source/goodway/goodway-web2/src/main/java/mksgroup/goodway/controller/DeliveryPlanController.java
@@ -1,11 +1,19 @@
 package mksgroup.goodway.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import mksgroup.goodway.entity.DeliveryBatch;
+import mksgroup.goodway.repository.DeliveryBatchRepository;
 
 @Controller
 public class DeliveryPlanController {
 
+	@Autowired
+	private DeliveryBatchRepository deliveryRepository;
 	
 	@RequestMapping(value = {"/deliveryplan","/deliveryplan/deliveryplan-index"})
 	public String goDeliveryPlanIndex() {
@@ -20,5 +28,12 @@ public class DeliveryPlanController {
 	@RequestMapping(value = {"/findway","/deliveryplan/delivery-result"})
 	public String goDeliveryPlanResult() {
 		return "deliveryplan/delivery-result";
+	}
+	
+	@GetMapping("/delivery/load-deliverylist")
+	@ResponseBody
+	public Iterable<DeliveryBatch> loadDelivery(){
+		Iterable<DeliveryBatch> deliveryList = deliveryRepository.findAll();
+		return deliveryList;
 	}
 }
