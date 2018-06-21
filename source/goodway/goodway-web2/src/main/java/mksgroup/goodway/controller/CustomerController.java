e6ec0ab7597c77b9c65cd007fca3d1c5632b2ae9
ZoroMochi <32060631+vndkubi@users.noreply.github.com>
2018-06-21 19:39:11 +0700

    update data mysql customer/search.html

--- source/goodway/goodway-web2/src/main/java/mksgroup/goodway/controller/CustomerController.java
+++ source/goodway/goodway-web2/src/main/java/mksgroup/goodway/controller/CustomerController.java
@@ -3,8 +3,14 @@
  */
 package mksgroup.goodway.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import mksgroup.goodway.entity.Customer;
+import mksgroup.goodway.repository.CustomerRepository;
 
 /**
  * This controller processes request /
@@ -13,6 +19,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class CustomerController {
 
+	@Autowired
+	private CustomerRepository customerRepository;
+	
     /**
      * Goto the index page.
      * @return
@@ -25,6 +34,19 @@ public class CustomerController {
     @RequestMapping("/customer/new")
     public String goCustomerNew() {
         return "customer/new";
+    }
+    
+    
+    /**Load danh sach customer
+     * 
+     * @return
+     */
+    @GetMapping("/customer/load-customer")
+    @ResponseBody
+    public Iterable<Customer> loadCustomer(){
+    	Iterable<Customer> customer = customerRepository.findAll();
+    	
+    	return customer;
     }
 
 }
