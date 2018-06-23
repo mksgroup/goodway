97ab3187c48f532d28b38de0107d825a5b030926
thachln <thachln@gmail.com>
2018-06-23 11:30:48 +0700

    Change GoodwayApplication into WebApp to build by maven command (package)

--- source/goodway/goodway-web2/src/main/java/mksgroup/goodway/GoodwayApplication.java
+++ source/goodway/goodway-web2/src/main/java/mksgroup/goodway/GoodwayApplication.java
@@ -2,11 +2,26 @@ package mksgroup.goodway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+//@SpringBootApplication
+//public class GoodwayApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(GoodwayApplication.class, args);
+//	}
+//}
 @SpringBootApplication
-public class GoodwayApplication {
+public class GoodwayApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GoodwayApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(GoodwayApplication.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(GoodwayApplication.class, args);
+    }
+
 }
