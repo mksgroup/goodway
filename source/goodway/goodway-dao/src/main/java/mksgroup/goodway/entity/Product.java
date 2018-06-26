bd38339c185c34eb300070eb54fa2072aa2e8ff0
NamTang <namkty96@gmail.com>
2018-06-26 10:06:09 +0700

    Add @JsonBackReference for List<OrderDetailProduct> - entity/Product.java fix product dataTable - fragments/product/search.html

--- source/goodway/goodway-dao/src/main/java/mksgroup/goodway/entity/Product.java
+++ source/goodway/goodway-dao/src/main/java/mksgroup/goodway/entity/Product.java
@@ -25,6 +25,8 @@ import javax.persistence.TemporalType;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 /**
  *
  * @author ThachLN
@@ -81,6 +83,7 @@ public class Product implements Serializable {
     @Column(name = "lastmodifiedby_username", length = 50)
     private String lastmodifiedbyUsername;
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "productId", fetch = FetchType.LAZY)
+    @JsonBackReference
     private List<OrderDetailProduct> orderDetailProductList;
 
     public Product() {
