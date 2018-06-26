1f0eae783110a25097355cd8b44b04532fd8b0e9
thachln <thachln@gmail.com>
2018-06-26 10:21:26 +0700

    Restore default configuration file application.properties. Add model and skeleton code Controller for Make Plan.

--- source/goodway/goodway-web2/src/main/java/mksgroup/goodway/controller/PlanController.java
+++ source/goodway/goodway-web2/src/main/java/mksgroup/goodway/controller/PlanController.java
@@ -1,11 +1,18 @@
 package mksgroup.goodway.controller;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import mksgroup.goodway.entity.DeliveryBatch;
+import mksgroup.goodway.entity.OrderMaster;
+import mksgroup.goodway.model.MakePlanModel;
 import mksgroup.goodway.repository.DeliveryBatchRepository;
 import mksgroup.goodway.repository.ProductRepository;
 
@@ -25,5 +32,24 @@ public class PlanController {
 	public String goDeliveryPlanSreach() {
 		return "plan/wizard";
 	}
+	
+    /**
+     * Lập kế hoạch giao hàng.
+     * @param model chứa thông tin đầu vào: danh sách mã đơn hàng, danh sách mã đội xe.
+     * @return danh sách các đợt giao hàng.
+     */
+    @RequestMapping(value = {"/plan/makePlan"})
+    public Iterable<DeliveryBatch> makeDeliveryPlanSreach(MakePlanModel model) {
+        List<DeliveryBatch> resultBatchs = new ArrayList<DeliveryBatch>();
+        
+        // Demo data
+        DeliveryBatch batch = new DeliveryBatch(1, "Chuyến 1", 1, new Date(), "ThachLN");
+        OrderMaster order = new OrderMaster(1, "DH1", 1, new Date(), new Date(), "ThachLN");
+        batch.setOrderId(order);
+        
+        resultBatchs.add(batch);
+        
+        return resultBatchs;
+    }
 	
 }
