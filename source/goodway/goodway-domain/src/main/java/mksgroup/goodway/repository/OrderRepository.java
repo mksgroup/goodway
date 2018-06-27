30cbfdf1708208023aa270289d5dc1745edd6720
vndkubi <caothaison936@gmail.com>
2018-06-27 17:02:21 +0700

    update query findByCustomerId.

--- source/goodway/goodway-domain/src/main/java/mksgroup/goodway/repository/OrderRepository.java
+++ source/goodway/goodway-domain/src/main/java/mksgroup/goodway/repository/OrderRepository.java
@@ -1,11 +1,18 @@
 package mksgroup.goodway.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import mksgroup.goodway.entity.Customer;
 import mksgroup.goodway.entity.OrderMaster;
 
 @Repository
 public interface OrderRepository extends CrudRepository<OrderMaster, Integer> {
- 
+	
+	@Query("SELECT o FROM OrderMaster o WHERE o.customerId = :customerId")
+	OrderMaster findByCustomerId(@Param("customerId") Customer customerId);
 }
