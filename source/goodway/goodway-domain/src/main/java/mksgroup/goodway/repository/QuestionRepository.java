1a3c528ed365388ee1cecf5ea85b6c5f8157bc66
NamTang <namkty96@gmail.com>
2018-06-28 09:55:34 +0700

    fix import link

--- source/goodway/goodway-domain/src/main/java/mksgroup/goodway/repository/QuestionRepository.java
+++ source/goodway/goodway-domain/src/main/java/mksgroup/goodway/repository/QuestionRepository.java
@@ -1,9 +1,9 @@
 package mksgroup.goodway.repository;
 
-import org.aspectj.weaver.patterns.TypePatternQuestions.Question;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import mksgroup.goodway.entity.Question;
 @Repository
 public interface QuestionRepository extends CrudRepository<Question, Integer>{
 
