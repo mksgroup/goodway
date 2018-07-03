df7598104ca4880746c6d97317441e68be7e6dba
NamTang <namkty96@gmail.com>
2018-07-03 22:20:21 +0700

    fix bug: "object references an unsaved transient instance - save the transient instance before flushing" when save OrderMaster.

--- source/goodway/goodway-web2/src/main/java/mksgroup/goodway/biz/impl/OrderProductBizImpl.java
+++ source/goodway/goodway-web2/src/main/java/mksgroup/goodway/biz/impl/OrderProductBizImpl.java
@@ -25,29 +25,53 @@ public class OrderProductBizImpl implements OrderProductBiz {
 
     @Override
     public List<OrderDetailProduct> updateOrderProducts(OrderMaster order, List<Integer> tobeDeletedIds) {
-        
+
         if (tobeDeletedIds != null) {
             tobeDeletedIds.forEach(t -> {
                 Product product = productRepository.findById(t).get();
                 orderProductRepository.delete(orderProductRepository.findByOrderIdAndProductId(order, product));
             });
         }
-        
+
         List<OrderDetailProduct> result = new ArrayList<OrderDetailProduct>();
-        
-        for (OrderDetailProduct o :   order.getOrderDetailProductList()) {
-            Product product = productRepository.findById(o.getProductId().getId()).get();
-            OrderDetailProduct orderProduct = orderProductRepository.findByOrderIdAndProductId(order, product);
-            if (orderProduct != null) {
-                orderProduct.setQuantity(o.getQuantity());       
-                
-                result.add(orderProductRepository.save(orderProduct));
-            } else {
-                o.setOrderId(order);
-                o.setProductName(product.getName());
-                o.setProductId(product);
-                
-                result.add(orderProductRepository.save(o));
+        OrderDetailProduct nullOrderProduct = null;
+
+        for (OrderDetailProduct o : order.getOrderDetailProductList()) {
+            // if (o != null) {
+            // Product product = productRepository.findById(o.getProductId().getId()).get();
+            // OrderDetailProduct orderProduct = orderProductRepository.findByOrderIdAndProductId(order, product);
+            // if (!orderProduct.equals(nullOrderProduct)) {
+            // orderProduct.setQuantity(o.getQuantity());
+            //
+            // result.add(orderProductRepository.save(orderProduct));
+            // } else {
+            // o.setOrderId(order);
+            // o.setProductName(product.getName());
+            // o.setProductId(product);
+            //
+            // result.add(orderProductRepository.save(o));
+            // }
+            // }
+            if (o != null) {
+                Product product = productRepository.findById(o.getProductId().getId()).get();
+
+                if (order.getId() != null) {
+                    OrderDetailProduct orderProduct = orderProductRepository.findByOrderIdAndProductId(order, product);
+
+                    if (orderProduct != null) {
+                        orderProduct.setQuantity(o.getQuantity());
+
+                        result.add(orderProduct);
+                    }
+                    
+                } else {
+
+                    o.setOrderId(order);
+                    o.setProductName(product.getName());
+                    o.setProductId(product);
+
+                    result.add(o);
+                }
             }
         }
 
