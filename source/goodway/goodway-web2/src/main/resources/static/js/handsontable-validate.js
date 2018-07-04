c742a22f4b50796237371678a157124a8d143027
thachln <thachln@gmail.com>
2018-07-04 18:18:59 +0700

    Using common js/handsontable-validate.js to validate int / float in handsontable.

---
+++ source/goodway/goodway-web2/src/main/resources/static/js/handsontable-validate.js
@@ -0,0 +1,71 @@
+
+// Mẫu để kiểm tra số thực
+var floatPattern = /^[\d]*[\.]{0,1}[\d]*$/;
+
+/**
+ * Check value is a positive integer.
+ * 
+ * @param value number to be checked
+ * @param callback function of caller.
+ */
+intValidator = function (value, callback) {
+	var pattern = /^[\d]*$/;
+	
+	if (pattern.test(value)) {
+	  callback(true);
+	} else {
+	  callback(false);
+	}
+};
+
+intValidator4 = function (value, callback) {
+	var pattern = /^[\d]*$/;
+	
+	if (pattern.test(value) && value.length <= 4) {
+	  callback(true);
+	} else {
+	  callback(false);
+	}
+};
+
+intValidator5 = function (value, callback) {
+	var pattern = /^[\d]*$/;
+	
+	if (pattern.test(value) && value.length <= 5) {
+	  callback(true);
+	} else {
+	  callback(false);
+	}
+};
+
+floatValidator = function (value, callback) {
+	if (floatPattern.test(value)) {
+	  callback(true);
+	} else {
+	  callback(false);
+	}
+};
+
+/**
+ * Kiểm tra số thực hợp lệ và phần nguyên có tối đa 4 kí số.
+ */
+floatValidator4 = function (value, callback) {
+	
+	
+	// trunk the int part
+	var dotIdx = value.indexOf(".");
+	
+	if (dotIdx > -1) {
+		var leftDot = value.substring(0, dotIdx - 1);
+	} else {
+		leftDot = value
+	}
+	
+	
+	if (floatPattern.test(leftDot) && leftDot.length <= 4) {
+	  callback(true);
+	} else {
+	  callback(false);
+	}
+};
+
