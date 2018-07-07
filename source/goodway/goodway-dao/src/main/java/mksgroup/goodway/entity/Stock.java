003de66a5c43487ec488954ddd31a02c46b45c91
thachln <thachln@gmail.com>
2018-07-07 18:31:21 +0700

    Add missing file

---
+++ source/goodway/goodway-dao/src/main/java/mksgroup/goodway/entity/Stock.java
@@ -0,0 +1,161 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mksgroup.goodway.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author ThachLN
+ */
+@Entity
+@Table(name = "goodway_stock")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Stock.findAll", query = "SELECT s FROM Stock s")
+    , @NamedQuery(name = "Stock.findById", query = "SELECT s FROM Stock s WHERE s.id = :id")
+    , @NamedQuery(name = "Stock.findByName", query = "SELECT s FROM Stock s WHERE s.name = :name")
+    , @NamedQuery(name = "Stock.findByCreated", query = "SELECT s FROM Stock s WHERE s.created = :created")
+    , @NamedQuery(name = "Stock.findByCreatedbyUsername", query = "SELECT s FROM Stock s WHERE s.createdbyUsername = :createdbyUsername")
+    , @NamedQuery(name = "Stock.findByLastmodified", query = "SELECT s FROM Stock s WHERE s.lastmodified = :lastmodified")
+    , @NamedQuery(name = "Stock.findByLastmodifiedbyUsername", query = "SELECT s FROM Stock s WHERE s.lastmodifiedbyUsername = :lastmodifiedbyUsername")})
+public class Stock implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+    @Column(name = "name", length = 128)
+    private String name;
+    @Basic(optional = false)
+    @Column(name = "created", nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date created;
+    @Basic(optional = false)
+    @Column(name = "createdby_username", nullable = false, length = 50)
+    private String createdbyUsername;
+    @Column(name = "lastmodified")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastmodified;
+    @Column(name = "lastmodifiedby_username", length = 50)
+    private String lastmodifiedbyUsername;
+    @JoinColumn(name = "address", referencedColumnName = "id", nullable = false)
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    private Address address;
+
+    public Stock() {
+    }
+
+    public Stock(Integer id) {
+        this.id = id;
+    }
+
+    public Stock(Integer id, Date created, String createdbyUsername) {
+        this.id = id;
+        this.created = created;
+        this.createdbyUsername = createdbyUsername;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public String getCreatedbyUsername() {
+        return createdbyUsername;
+    }
+
+    public void setCreatedbyUsername(String createdbyUsername) {
+        this.createdbyUsername = createdbyUsername;
+    }
+
+    public Date getLastmodified() {
+        return lastmodified;
+    }
+
+    public void setLastmodified(Date lastmodified) {
+        this.lastmodified = lastmodified;
+    }
+
+    public String getLastmodifiedbyUsername() {
+        return lastmodifiedbyUsername;
+    }
+
+    public void setLastmodifiedbyUsername(String lastmodifiedbyUsername) {
+        this.lastmodifiedbyUsername = lastmodifiedbyUsername;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Stock)) {
+            return false;
+        }
+        Stock other = (Stock) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "mksgroup.goodway.entity.Stock[ id=" + id + " ]";
+    }
+    
+}
